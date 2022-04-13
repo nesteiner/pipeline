bcf0fbe609a55489de256a018234b4bf5cf85733
nesteiner <steiner3044@163.com>
2022-04-13 14:50:46 +0800

    first commit, julia pipeline

---
+++ src/pipeline.jl
@@ -0,0 +1,51 @@
+#=
+@pipeline variable begin
+    x -> x + 1
+    x -> x * 2
+    x -> x / 3
+end
+
+=#
+
+# 1 |>
+#     x -> x + 1 |>
+#     x -> x * 2 |>
+#     x -> x / 3
+
+macro pipeline(variable, beginExpr::Expr)
+    exprs = filter(x -> isa(x, Expr), beginExpr.args) |>
+        nums -> convert(Vector{Expr}, nums)
+    firstFunc = first(exprs)
+    
+    if length(exprs) == 1
+        quote
+	    @pipeline $firstFunc($variable)
+        end
+    else
+        restFuncs = exprs[2:end]
+
+        quote
+	    @pipeline $firstFunc($variable) $restFuncs
+        end
+    end
+end
+
+macro pipeline(variable, funcExprs::Vector{Expr})
+    firstFunc = first(funcExprs)
+    
+    if length(funcExprs) == 1
+        quote
+	    @pipeline $firstFunc($variable)
+        end
+    else
+        restFuncs = funcExprs[2:end]
+
+        quote
+	    @pipeline $firstFunc($variable) $restFuncs
+        end
+    end
+end
+
+macro pipeline(variable)
+    variable
+end
